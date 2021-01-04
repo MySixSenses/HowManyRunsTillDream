1b27cf6ef1b4a628b780211e73ecb33a98070b55
MySixSenses <66916362+MySixSenses@users.noreply.github.com>
2021-01-04 13:33:54 -0800

    Increased Efficiency by reducing input to one in one thousand, and some QoL changes

--- HowManyRunsTillDream.cpp
+++ HowManyRunsTillDream.cpp
@@ -1,19 +1,21 @@
 // HowManyRunsTillDream.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <ctime>
 #include <iostream>
 static int BlazesKilled = 305;
 static int GoldBartered = 262;
 static int BlazeDrops[2] = { 0, 1 };
-static int PearlBarters[423] = {2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3 };
+static int PearlBarters[423] = {2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3};
 static int DreamDrops = 211;
 static int DreamPearls = 42;
 int LastDrops = 0;
 int LastPearls = 0;
+int BestBlazeRunInLast1000 = 0;
+int BestPearlRunInLast1000 = 0;
 long long CurrentRun = 0;
+
 int main(){
 	srand(time(NULL)); //initialize the random seed
-
 	while (LastDrops < DreamDrops || LastPearls < DreamPearls) {
 		LastDrops = 0;
 		LastPearls = 0;
@@ -32,9 +34,18 @@ int main(){
 				LastPearls += 3;
 			}
 		}
-		printf("Pearls: %d Blaze Rods: %d\n", LastPearls, LastDrops);
+		if (BestBlazeRunInLast1000 < LastDrops) {
+			BestBlazeRunInLast1000 = LastDrops;
+		} else if (BestPearlRunInLast1000 < LastPearls) {
+			BestPearlRunInLast1000 = LastPearls;
+		}
+		if (CurrentRun % 1000 == 0) {
+			printf("Best Blaze run had %d rods out of 305 and Best Pearl Run had %d pearls out of 262.\n", BestBlazeRunInLast1000, BestPearlRunInLast1000);
+			BestBlazeRunInLast1000 = 0;
+			BestPearlRunInLast1000 = 0;
+		}
 	}
-	printf("Luck equaled or exceeded Dream with %d blaze rods of 305 and %d ender pearls of 262. This is the %dth iteration", LastDrops, LastPearls, CurrentRun);
+	printf("Luck equaled or exceeded Dream with %d blaze rods of 305 and %d ender pearls of 262. This is the %I64uth iteration", LastDrops, LastPearls, CurrentRun);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
